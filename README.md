dba4298438dae328cb3a49f8d7bb36f490aa229a
Qetsia Nkulu <qetsiankulu@gmail.com>
2025-04-15 16:59:24 -0700

    Version 1.0 completed

--- README.md
+++ README.md
@@ -1,132 +1,45 @@
-# SEA Stage 2 - Data Catalog Project
+*Dream Travel Destinations*
 
-This repository contains the instructions, requirements, and starter code for Stage Two of the Snap Engineering Academy application process.
+Submitted by: **Qetsia Nkulu**
 
----
+**Dream Travel Destinations** is a simple catalog webpage that shows various travel destinations that I would like to visit one day. 
 
-## 📚 Table of Contents
+Time spent: **10+** hours spent in total
 
-- [Your Task](#your-task)
-- [Requirements](#requirements---your-catalog-website-should)
-- [Getting Started](#getting-started)
-- [Submitting](#submitting)
-- [❓ Frequently Asked Questions (FAQ)](#-frequently-asked-questions-faq)
+## Features
 
----
+The following **required** functionality is completed:
 
-## 🎯 Your Task
+- [x] User can add new entries to the site 
+- [x] User can delete entries
+- [x] User can filter destinations by continents 
+- [x] User can see destinations of multiple continents at once 
 
-### <p align="center">Create a "catalog" website for something you're passionate about.</p>
 
-> 📝 **Note:**  
-> If you have not used GitHub or programmed a website with JavaScript before, that's OK! Part of the challenge is figuring out things you're not familiar with.
+## Known Bugs
+- If a continent is selected, deleting all the entries associated with that continent crashes the webpage 
+- When new entry is added, it does not appear when selecting the continent filter (i.e. creating en entry for Tokyo, Japan, Asia does not appear when 'Asia' filter is selected)
 
----
+## Potential Features 
+- Adding an 'Edit' button that allows users to edit an entry and modify the car image 
+- Add a filter for parks 
 
-## ✅ Requirements - Your Catalog Website Should...
 
-- Show off your understanding of basic data structures: **arrays** and **objects**.
-- Display a **substantial amount of interesting data** in a "catalog". You can look online for datasets or create your own.  
-  Your data should be easy to find in your source code—either in variables at the top of `scripts.js` or imported from a file.
-- Include **two or more features** that operate on your data and modify how it's displayed. Examples:
-  - Filtering
-  - Searching
-  - Sorting
-  - Updating
-  - Adding/removing entries  
-    Think about what users might want to do while using your site!
-- Look polished ✨. Use **HTML** and **CSS** to make your data easy to read and visually appealing.
-- Be built from this **starter code**—you can change anything you want, but build on top of it.
-- Be an **original** project. Please do not submit something previously created for a class, internship, or client.  
-  You are encouraged to use online resources, but **make sure you understand every line of code** in your project.
+## Video Walkthrough
+<div>
+    <a href="https://www.loom.com/share/51df3c9422f44ddd8d4d29ebacd0031e">
+      <p>Qetsia's Dream Travel Destinations-1.0 - Watch Video</p>
+    </a>
+    <a href="https://www.loom.com/share/51df3c9422f44ddd8d4d29ebacd0031e">
+      <img style="max-width:300px;" src="https://cdn.loom.com/sessions/thumbnails/51df3c9422f44ddd8d4d29ebacd0031e-eba3bb1a7d92ebcc-full-play.gif">
+    </a>
+</div>
 
----
 
-## 🛠️ Getting Started
 
-1. **Create a GitHub account** if you haven't already.
-2. Click the green **"Use as Template"** button in the upper right corner, then choose **"Create a new repository"**:  
-   a. Choose yourself as the owner  
-   b. Give your repository a name  
-   c. Click **"Create Repository"**
-3. Copy or download the files to your own computer.
-4. Open and edit the website using a text editor:
-   - Modify `index.html`, `style.css`, and `scripts.js`.
-   - To preview, open `index.html` in a web browser (double-click it).
-   - You should see something like this:
 
-<img height="300" alt="Screenshot of catalog example" src="https://github.com/Snap-Engineering-Academy-2023/rn_lab1/assets/7607483/fdd57236-50fe-48ca-956d-d9b4b12db038">
 
----
 
-## 🚀 Submitting
 
-1. **Publish your website to the internet!**  
-   We recommend using [GitHub Pages](https://docs.github.com/en/pages/getting-started-with-github-pages/creating-a-github-pages-site#creating-your-site).
-2. **Test the published version.**  
-   Make sure everything works properly before submitting.
-3. **Update your GitHub repository** so it reflects the latest version of your project.
-4. **Submit** both:
-   - The **URL** to your published website
-   - The **link** to your GitHub repository  
-     …via the Google Form linked in your email.
 
----
 
-## ❓ Frequently Asked Questions (FAQ)
-
-### ❄️ Is it OK that my catalog resets when I refresh the page?
-
-**Yes!** That's exactly what the starter code does too. You don't need to worry about preserving data after a page refresh.
-
----
-
-### 💻 Can I copy bits of code from online resources?
-
-**Yes, absolutely!** You should search for and use **small chunks** of code.  
-For example:
-
-- ✅ Copying code to create a dropdown menu is fine.
-- ❌ Copying a full “filter data by date” feature is not.
-
-Be thoughtful about what you borrow!
-
----
-
-### 🤖 Can I use generative AI (like ChatGPT or Copilot)?
-
-**Partially.**
-
-- ✅ You _may_ use AI tools to help write **HTML** and **CSS**.
-- ❌ You _may not_ use AI to write **JavaScript**.
-
-HTML/CSS can be tricky to get right, and it's okay to get help making things look nice.  
-But JavaScript is where you show your own logic and understanding of data!
-
----
-
-### 🧰 Can I use a different template?
-
-**Nope!**  
-You must use the provided starter code, though you're free to customize it however you like.  
-You _can_ copy **small chunks** from other templates if needed.
-
----
-
-### 🛠️ Can I use a framework like React, Vue, Bootstrap, or Tailwind?
-
-**Nope!**  
-This project is for folks new to web dev. Frameworks do a lot of heavy lifting and hide the logic we're asking you to demonstrate.  
-SEA will teach you frameworks later—stick to **vanilla HTML, CSS, and JS** for now.
-
----
-
-### 🌐 Can I use APIs?
-
-**Nope!**  
-APIs can add complexity beyond the scope of this challenge.  
-If you really want to use data from an API, **save it to a file** (like `.json` or `.csv`) and use that instead.
-
----
-
-> 💬 **Have any questions?** Drop them on the [Padlet here](https://padlet.com/arlenschallenge/2025-snap-engineering-academy-stage-2-project-assessment-que-ndmqkef3wxt8sh72) and we'll get back to you!
--- scripts.js
+++ scripts.js
@@ -1,29 +1,3 @@
-/**
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- *
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your
- *    browser and make sure you can see that change.
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- *
- */
-
-
 // JSON data for cities to be displayed
 const cities = [ 
   { 
@@ -49,57 +23,56 @@ const cities = [
       "country": "Brazil",
       "continent": "South America",
       "image": "https://lp-cms-production.imgix.net/2022-08/iStock-1322713774.jpg"
-  }
+  }, 
+  {
+    "city": "Reykjavík",
+    "country": "Iceland",
+    "continent": "Europe",
+    "image": "https://images.ctfassets.net/w65k7w0nsb8q/6RINnJSwvuUo88s8Mgc4s4/46f766f05011c6b85e0f7bdd36d6cf12/Blue_Lagoon_Northernlights.jpg?w=3840&q=75&fm=webp"
+  }, 
+  {
+    "city": "Santorini",
+    "country": "Greece",
+    "continent": "Europe",
+    "image": "https://i0.wp.com/www.wanderfullyrylie.com/wp-content/uploads/2024/03/Dream-Destinaitons-To-Add-To-Your-Bucket-List-Travel-Wanderfully-Rylie-Weekend-Adventure-Adventurer-Blog-5.png?resize=1536%2C864&ssl=1"
+  }, 
+  {
+    "city": "Nassau",
+    "country": "Bahamas",
+    "continent": "North America",
+    "image": "https://assets.dmagstatic.com/wp-content/uploads/2023/07/Atlantis-Resorts-Nassau-Bahamas-exterior-view-1200x800.jpg"
+  }, 
+  {
+    "city": "Sasaab",
+    "country": "Kenya",
+    "continent": "Africa",
+    "image": "https://cdn.shortpixel.ai/spai/q_glossy+ret_img+to_auto/www.discoverafrica.com/wp-content/uploads/wetu/19848/sasaab-2019-1254-1800x1080.jpg"
+  }, 
+  {
+    "city": "Sydney",
+    "country": "Australia",
+    "continent": "Oceania",
+    "image": "https://images.squarespace-cdn.com/content/v1/55ee34aae4b0bf70212ada4c/1577545161018-1F9Z9ZZQG9JO2O4WCWQX/keith-zhu-qaNcz43MeY8-unsplash+%281%29.jpg?format=1500w"
+  }, 
+  {
+    "city": "Tahiti",
+    "country": "French Polynesia",
+    "continent": "Oceania",
+    "image": "https://www.tahitilegends.com/images/territories/tahiti.jpg"
+  }, 
+  {
+    "city": "Rio de Janeiro",
+    "country": "Brazil",
+    "continent": "South America",
+    "image": "https://media.timeout.com/images/105482246/image.jpg"
+  }, 
 
 ]
 
-// This function creates a new city 
-function createCity() { 
-  const input = document.getElementById("destination"); // get the destination info from input field 
-  const value = input.value.trim()                      // remove extra whitespace (e.g. "Tokyo, Japan, Asia")
+// Global set to keep track which continents are selected
+let selectedContinents = new Set(); 
 
-  const parts = value.split(",");  // Array ["Tokyo", "Japan", "Asia"]
-
-
-  const [city, country, continent] = parts; // use object destructing to retrieve data from user input
-
-  const newCity = { 
-    city, 
-    country, 
-    continent, 
-    image: "https://i0.wp.com/port2flavors.com/wp-content/uploads/2022/07/placeholder-614.png?fit=1200%2C800&ssl=1.jpg"
-  }; 
-
-  // Add to cities array
-  cities.push(newCity); 
-
-  // Re-render cards
-  showCards(); 
-
-  // Clear the input
-  input.value = ""; 
-}
-
-// This function adds cards the page to display the data in the array
-function showCards() {
-  const cardContainer = document.getElementById("card-container"); // select the container
-  const templateCard = document.querySelector(".card");            // select the template card
-
-  cardContainer.innerHTML = "";   // clear the container 
-
-  // loop through the cities array
-  // for each city: 
-  //  (1) clone the template card 
-  //  (2) update the cloned card with city info 
-  //  (3) append updated card to the container 
-  cities.forEach(city => { 
-    const newCard = templateCard.cloneNode(true);     // Copy the template card 
-    editCardContent(newCard, city);               // edit image and text content 
-    cardContainer.appendChild(newCard);             // Add the new card to the container 
-  });
-}
-
-// This function manages the states (hover, click, etc) of the continent filter
+// This function manages the states (hover, click, etc) of the continents filter 
 function handleContinentStates() { 
   // an array of all the `continent-items`
   const continentItems = document.querySelectorAll('.continent-item'); 
@@ -108,39 +81,118 @@ function handleContinentStates() {
     const img = item.querySelector('.continent-image'); // get the image element
     const text = item.querySelector('.continent-text'); // get the text element
 
+    // helper function to set the hover style 
+    const setHoverStyle = () => { 
+      img.src = img.getAttribute('data-hover'); 
+      text.style.color = '#000'
+    }; 
+
+    // helper function to set the default style 
+    const setDefaultStyle = () => { 
+      img.src = img.getAttribute('data-default'); 
+      text.style.color = '#ccc'
+    }
+
+
     // Event listener for when the item is being hovered
     item.addEventListener('mouseenter', () => { 
-      if (!item.classList.contains('selected')) {        // check if the item 
-        img.src = img.getAttribute('data-hover');        // change the image source
-        text.style.color = '#000';                      // change the color 
+      if (!item.classList.contains('selected')) {        // check if the item is not selected 
+        setHoverStyle();   
       }
-    });
+    }); 
 
     // Event listener for when the item is not being hovered anymore
     item.addEventListener('mouseleave', () => { 
-      if (!item.classList.contains('selected')) { 
-        img.src = img.getAttribute('data-default'); // revert back to the original image source
-        text.style.color = '#ccc';                  // revert back to the original color
+      if (!item.classList.contains('selected')) {   // check if item is not selected 
+        setDefaultStyle(); 
       }
     }); 
 
     // Event listener for when an item is clicked
     item.addEventListener('click', () => { 
-      item.classList.toggle('selected');       // toggle the "selected" class on the `continent-item` when its clicked 
+      item.classList.toggle('selected');          // toggle the "selected" on the item
+      const continent = text.textContent.trim();  // get the continent that was selected 
       
-      // if "selected" is toggled on, keep the styling from the hover state
-      // else, revert back to the default styling 
+      // if "selected" is toggled on, keep the styling from the hover state and add the current continent to selectedContinents
+      // else, revert back to the default styling and removed the continent from selectedContinents
       if (item.classList.contains('selected')) { 
-        img.src = img.getAttribute('data-hover');       
-        text.style.color = '#000';                      
-       } else { 
-          img.src = img.getAttribute('data-default'); 
-          text.style.color = '#ccc';                    
+        setHoverStyle(); 
+        selectedContinents.add(continent);
+
+      } else { 
+        setDefaultStyle();  
+        selectedContinents.delete(continent);              
       }
+
+      showCards(); // re-render the cards after deletion
     });
+
   });
+}
 
+// This function creates a new city 
+function createCity() { 
+  const input = document.getElementById("destination"); // get the destination info from input field 
+  const value = input.value.trim()                      // remove extra whitespace (e.g. "Tokyo, Japan, Asia")
 
+  const parts = value.split(",");  // Array ["Tokyo", "Japan", "Asia"]
+  const [city, country, continent] = parts; // use object destructing to retrieve data from user input
+
+  const newCity = { 
+    city, 
+    country, 
+    continent, 
+    image: "https://i0.wp.com/port2flavors.com/wp-content/uploads/2022/07/placeholder-614.png?fit=1200%2C800&ssl=1.jpg" // placeholder image 
+  }; 
+
+  cities.push(newCity); // Add to cities array
+  showCards();          // Re-render cards with filtering applied
+  input.value = "";     // Clear the input
+}
+
+// This function adds cards the page to display the data in the array
+function showCards() {
+  const cardContainer = document.getElementById("card-container"); // select the container
+  const templateCard = document.querySelector(".card");            // select the template card
+
+  cardContainer.innerHTML = "";   // clear the container     
+
+  // Get the filtered list based on the selected continents 
+  const filteredCities = getFilteredCities(); 
+
+  // loop through the cities array
+  // for each city: 
+  //  (1) clone the template card 
+  //  (2) update the cloned card with city info 
+  //  (3) set up the event listener for the delete button 
+  //  (3) append updated card to the container 
+  filteredCities.forEach((city, index) => {           // include the index in the loop 
+    const newCard = templateCard.cloneNode(true);     // Copy the template card 
+    editCardContent(newCard, city);                   // edit image and text content 
+
+    // Set the data-index on the delete button
+    const deleteButton = newCard.querySelector('.card-delete-btn');    // get the delete button for the card
+    deleteButton.setAttribute('data-index', index);                   // set the index for that card 
+
+    // Attach event listener for deleting the card 
+    //  (1) get the index of the card 
+    //  (2) get the city that is to be removed 
+    //  (3) find and remove the city in the original array 
+    //  (4) re-render the cards
+    deleteButton.addEventListener('click', (e) => { 
+      const cardIndex = e.currentTarget.getAttribute('data-index');
+      const cityToRemove = filteredCities[cardIndex]; 
+      
+      const originalIndex = cities.findIndex(c => c.city == cityToRemove.city)
+      if (originalIndex !== -1) { 
+        cities.splice(originalIndex, 1) // Remove the city from the original `cities` array
+      }
+
+      showCards(getFilteredCities());   // Re-render the cards with updated list
+    }); 
+
+    cardContainer.appendChild(newCard); // Add the new card to the container 
+  });
 }
 
 // This function updates the card info
@@ -160,10 +212,14 @@ function editCardContent(card, city) {
   <li>${city.country}</li> 
   <li>${city.continent}</li> 
   `; 
-
-  // console.log for debugging
-  console.log("new card:", city.city, "- html: ", card);
 }
+
+// This is a helper function that returns the cities from the continent selected 
+function getFilteredCities() { 
+  if (selectedContinents.size === 0) return cities; // no selected continents, show all the cities in the array 
+  return cities.filter(city => selectedContinents.has(city.continent))  // filter the cities who continent matches those in the selected continents
+}
+
 // When the page is first loaded, 
 //  (1) Handle the states for the Continent filter 
 //  (2) Show the cards for each city
@@ -174,4 +230,3 @@ document.addEventListener("DOMContentLoaded", () => {
   const addButton = document.querySelector('.add-destination-btn'); 
   addButton.addEventListener('click', createCity)
 }); 
-
